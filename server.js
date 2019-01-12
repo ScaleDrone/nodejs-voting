0ffd83c6dc615e1f30bc2b62a8eac3920cf1a4f3
kennypee <ekeneeze3@gmail.com>
2019-01-12 13:09:19 +0100

    Publishing votes from the server

--- server.js
+++ server.js
@@ -1,48 +1,42 @@
-
-require('dotenv').config();
-const express = require('express');
-const bodyParser = require('body-parser');
-
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
-const port = process.env.PORT || 4000;  
+const port = process.env.PORT || 4000;
 
-// const Scaledrone = require('scaledrone-node-push');
-// const sd = new Scaledrone({
-//   channelId: process.env.CHANNEL_ID,
-//   secretKey: process.env.SECRET_KEY
-// });
-
+const Scaledrone = require("scaledrone-node-push");
+const sd = new Scaledrone({
+  channelId: process.env.CHANNEL_ID,
+  secretKey: process.env.SECRET_KEY
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header("Access-Control-Allow-Origin", "*");
   res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
   );
   next();
 });
 
+app.post("/vote", (req, res) => {
+  const { body } = req;
+  const room = "live-votes";
+  const response = { playerId: body.vote.player_id };
 
-app.post('/vote', (req, res) => {
-    const { body } = req
-    console.log(body)
-    const reqArray = Object.values(body)
-    const votedPlayer = reqArray.map(player => player.votingFor)
-    res.json(votedPlayer)
-//     const room = 'votes';
-//     sd.publish(room, votedPlayer, error => {
-//     // check for errors
-//     if(error){
-//         console.log(error)
-
-//     }else res.json(votedPlayer)
-    
-// });
-
-
+  sd.publish(room, response.playerId, error => {
+    // check for errors
+    if (error) {
+      console.log(error);
+    } else {
+      res.json({
+        player_id: body
+      });
+    }
   });
+});
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
--- src/App.js
+++ src/App.js
@@ -1,29 +1,28 @@
-import React, { Component } from 'react';
-import './App.css';
-import Player from './components/players'
-import cr7 from '../src/img/ronaldo.jpg'
-import lm10 from '../src/img/lm10.jpg'
-import pogba from '../src/img/paul.jpg'
-import axios from 'axios'
-//import Scaledrone from 'scaledrone-node-push'
+import React, { Component } from "react";
+import "./App.css";
+import Player from "./components/players";
+import cr7 from "../src/img/ronaldo.jpg";
+import lm10 from "../src/img/lm10.jpg";
+import pogba from "../src/img/paul.jpg";
+import axios from "axios";
 
 const playerData = [
   {
-    name: 'Ronaldo',
+    name: "Ronaldo",
     goals: 30,
     votes: 0,
     id: 1,
     image: cr7
   },
   {
-    name: 'Messi',
+    name: "Messi",
     goals: 8,
     votes: 0,
     id: 2,
     image: lm10
   },
   {
-    name: 'Pogba',
+    name: "Pogba",
     goals: 26,
     votes: 0,
     id: 3,
@@ -31,87 +30,51 @@ const playerData = [
   }
 ];
 
-function randomName() {
-  const adjectives = ["autumn", "hidden", "bitter", "misty", "silent", "empty", "dry", "dark", "summer", "icy", "delicate", "quiet", "white", "cool", "spring", "winter", "patient", "twilight", "dawn", "crimson", "wispy", "weathered", "blue", "billowing", "broken", "cold", "damp", "falling", "frosty", "green", "long", "late", "lingering", "bold", "little", "morning", "muddy", "old", "red", "rough", "still", "small", "sparkling", "throbbing", "shy", "wandering", "withered", "wild", "black", "young", "holy", "solitary", "fragrant", "aged", "snowy", "proud", "floral", "restless", "divine", "polished", "ancient", "purple", "lively", "nameless"];
-  const nouns = ["waterfall", "river", "breeze", "moon", "rain", "wind", "sea", "morning", "snow", "lake", "sunset", "pine", "shadow", "leaf", "dawn", "glitter", "forest", "hill", "cloud", "meadow", "sun", "glade", "bird", "brook", "butterfly", "bush", "dew", "dust", "field", "fire", "flower", "firefly", "feather", "grass", "haze", "mountain", "night", "pond", "darkness", "snowflake", "silence", "sound", "sky", "shape", "surf", "thunder", "violet", "water", "wildflower", "wave", "water", "resonance", "sun", "wood", "dream", "cherry", "tree", "fog", "frost", "voice", "paper", "frog", "smoke", "star"];
-  const adjective = adjectives[Math.floor(Math.random() * adjectives.length)];
-  const noun = nouns[Math.floor(Math.random() * nouns.length)];
-  return adjective + noun;
-}
-
 class App extends Component {
-
-  constructor(){
-    super()
-    this.drone = new window.Scaledrone("vIh5lXOnewFxNIeC", {
-      data: this.state.users
-    });
-    this.drone.on('open', error => {
-      if (error) {
-        return console.error(error);
-      }
-      console.log("Connected to room")
-      const user = {...this.state.users};
-      user.id = this.drone.clientId;
-      this.setState({user});
-      console.log(user)
-    });
-      const room = this.drone.subscribe("votes");
-      room.on('data', data => console.log('Received data:', data));
-  }
-
   state = {
-    playerDetails: [],
-    users:{
-      username: randomName()
-    },
-    votes: ''
+    playerDetails: []
+  };
 
+  constructor() {
+    super();
+    this.drone = new window.Scaledrone("AfdKeFBcrOpY48AO");
+    this.drone.on("open", error => {
+      if (error) {
+        return console.log(error);
+      }
+    });
+
+    const room = this.drone.subscribe("live-votes");
+    room.on("data", data => {
+      this.state.playerDetails.map(player => {
+        if (player.name === data) {
+          return Object.assign({}, player, {
+            votes: (player.votes += 5)
+          });
+        } else {
+          return player;
+        }
+      });
+
+      this.setState({
+        playerDetails: this.state.playerDetails
+      });
+    });
   }
 
   componentDidMount() {
-    this.setState({ playerDetails: playerData })
-
+    this.setState({ playerDetails: playerData });
   }
-
   handleEvent = playerId => {
-    //console.log(playerId)
+    const vote = { player_id: playerId };
 
-    const castVote = {
-      votingFor: playerId,
-    }
-
-    axios.post(`http://localhost:4000/vote`, {castVote}).then(
-      res =>{
-        //console.log(res.data[0])
-        const message = res.data[0];
-        this.drone.publish({
-          room: "votes",
-          message
-        });
-       
-      }
-    )
-
-    this.state.playerDetails.map(player => {
-      if (player.name === playerId) {
-        return Object.assign({}, player, {
-          votes: player.votes++
-        });
-      } else {
-        return player;
-      }
+    axios.post("http://localhost:4000/vote", { vote }).then(response => {
+      console.log(response);
     });
-    //console.log(this.state.playerDetails)
-    //console.log(updatedList)
-    this.setState({
-      playerDetails: this.state.playerDetails
-    });
-    }
+  };
 
   render() {
-    return playerData.map(player =>
-
+    return playerData.map(player => (
       <Player
         key={player.id}
         name={player.name}
@@ -120,7 +83,8 @@ class App extends Component {
         votes={player.votes}
         id={player.id}
         onVoteCasted={this.handleEvent}
-      />);
+      />
+    ));
   }
 }
 
